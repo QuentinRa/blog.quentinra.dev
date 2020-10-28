4b3d2272d75f048001b1de044614ffeccb18763a
Quentin Ra <quentin.ramsamy.cf@gmail.com>
2020-10-28 21:55:26 +0100

    test conf.py without pdf build

--- conf.py
+++ conf.py
@@ -33,7 +33,7 @@ release = '1.2.44'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 import sphinx_rtd_theme
-extensions = ['recommonmark', "sphinx_rtd_theme", 'sphinx.ext.graphviz','rst2pdf.pdfbuilder']
+extensions = ['recommonmark', "sphinx_rtd_theme", 'sphinx.ext.graphviz']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
